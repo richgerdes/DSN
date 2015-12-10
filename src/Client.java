70aa90c5de153fd77bf2bdb95258a629c48a7e01
Rich GerdeS <richgerdes21@gmail.com>
2015-12-10 15:43:01 -0500

    fix files

--- src/Client.java
+++ src/Client.java
@@ -1,89 +1,103 @@
-import java.io.BufferedReader;
-import java.io.Console;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
+import java.security.*; 
 
-import moses.member.Member;
-import moses.member.Receiver;
-import moses.security.LGICert;
-import moses.security.Secu;
-import moses.security.certCreation;
-import moses.util.Const;
+import moses.member.*;
+import moses.security.*;
+import moses.controlState.*;
+import moses.util.*;
 
+public class Client implements Agent{
+	
+	SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+	
+    public void run (String[] args) throws Exception { 
+        if(args.length < 2) {
+            System.out.println("> java Client controller_name controller_port");
+            return;
+        }
+        
+        String controller_name = args[0];
+        String controller_port = args[1];
+        String law_path = "./echo.law";
+        String agent_name = "";
+        String agent_secret = "";
+        
 
-public class Client {
-	 public void run (String[] args) throws Exception { 
-		 
-		 
-			if(args.length < 2) {
-				System.out.println("> java Client controller_name controller_port");
-				return;
-			}
-			
-			String controller_name = args[0];
-			int controller_port = Integer.parseInt(args[1]);
-			String law_path = "/the/law/isStatic.law";
-			
-			Console console = System.console();
-			
-			if(console == null){
-				System.out.println("FAULT: Could not detect console... Exiting...");
-				System.exit(-1);
-			}
-			
-			
-			
-			System.out.println("Please Enter your username: ");
-			String identity = new String(System.console().readPassword());
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please Log in.");
+        while(agent_name.equals("")){
+    		System.out.print("username: ");
+        	agent_name = in.readLine();
+        	if(agent_name.equals(""))
+        		System.out.println("Invalid Username.");
+        }
+        
+        while(agent_secret.equals("")){
+    		System.out.print("password: ");
+    		agent_secret = in.readLine();
+        	if(agent_secret.equals(""))
+        		System.out.println("Invalid Password.");
+        }
 
-			while(identity == null || identity.equals("")){
-				System.out.println("Invalid username. Please enter your username: ");
-				identity = new String(System.console().readPassword());
-			}
-			
-			
-			System.out.println("Please enter your password: ");
-			String secret = new String(System.console().readPassword());
-
-			while(secret == null || secret.equals("")){
-				System.out.println("Invalid password. Please enter your password: ");
-				secret = new String(System.console().readPassword());
-			}
-			
-			ClientAgent agent = new ClientAgent(identity, secret, law_path, controller_name, controller_port );
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			
-			System.out.print("> ");
-			try{
-				String raw = null;
-				while ((raw  = in.readLine()) != null) {
-					if (raw.equals("logout")) {
-						agent.logout();
-						System.out.println("Signing... Out");
-					   	break;
-					}
-
-					String[] cmd = raw.split(" ");
-					String[] cmd3 = raw.split(" ", 3);
-					String[] cmd4 = raw.split(" ", 3);
-					
-					switch(cmd[0]){
-					case "set":
-						if(!(cmd[1].equals("private") || cmd[1].equals("public"))){
-							System.out.println("Invalid visibility type: " + cmd[1]);
-							break;
-						}
-					}
+        FileInputStream law_stream  = new FileInputStream(law_path);
+        byte[] law_byte = new byte[law_stream.available()];
+        law_stream.read(law_byte);
+        String law_content = new String(law_byte);
 
 
-					System.out.print("> ");
-				}
+        Member member = new Member(law_content, Const.IMM_LAW, controller_name, Integer.parseInt(controller_port), agent_name);
 
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
+        member.adopt(agent_secret,"someargument");
+
+        new Receiver(this, member).start();
+
+        System.out.println("Login Complete.");
+        System.out.println("Type exit to quit.");
+        
+        System.out.print("> ");
+        
+        while (true) {
+            try{
+                String raw_command  = in.readLine();
+                if (raw_command.equals("exit")) {
+                    member.close();
+                    System.out.println("Logging off...");
+                    System.exit(0);
+                }
+
+                if(!raw_command.equals(""))
+                	member.send_lg(raw_command, member.longName);
+                
+                System.out.print("> ");
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void processRequest(Member member, String message, String destination) {
+        System.out.println("\nSent to " + destination + ": " + message +"\n>");
+    }
+
+    public void processReply(Member member, String reply) {
+        System.out.println("\n" + format.format(new Date()) + ": " + reply +"\n>");
+    }
+
+    public void processReply(Member member, byte[] breply) {
+        System.out.println("\n" + format.format(new Date()) + ": " + breply +"\n>");
+    }
+
+    public void processReply(Member member, Object oreply) {
+        System.out.println("\n" + format.format(new Date()) + ": " + oreply +"\n>");
+    }
+
+    public static void main (String[] args) throws Exception {
+        Client agent = new Client();
+        String[] clientArgs = {"192.168.94.202","9000"};
+        agent.run(clientArgs);
+    }
 }
